e35d0d733a719ecfdc73ededb97de5e968f7e3be
Jcho114 <joseph.cho20@gmail.com>
2025-09-02 10:47:02 -0400

    chore: enabled ghostty in home-manager (empty for now)

--- home.nix
+++ home.nix
@@ -18,6 +18,10 @@
     };
   };
 
+  programs.ghostty = {
+    enable = true;
+  };
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
