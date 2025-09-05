6a30d009ea792ff1b13b03082bb136f9a256a145
Jcho114 <joseph.cho20@gmail.com>
2025-09-04 20:09:43 -0400

    chore: formatting fix

--- home.nix
+++ home.nix
@@ -19,13 +19,13 @@
     plugins = [
       {
         name = "powerlevel10k";
-	src = pkgs.zsh-powerlevel10k;
-	file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
       {
         name = "powerlevel10k-config";
-	src = builtins.toString ./.;
-	file = "p10k.zsh";
+        src = builtins.toString ./.;
+        file = "p10k.zsh";
       }
     ];
   };
