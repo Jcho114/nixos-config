47e04931a5e8028c392ba6b655620ef6859b29c2
Jcho114 <joseph.cho20@gmail.com>
2025-09-05 10:57:25 -0400

    chore: updated ghostty config

--- ghostty.nix
+++ ghostty.nix
@@ -6,5 +6,11 @@
 
   home.file."./.config/ghostty/config".text = ''
     command = zsh --login
+    font-size = 12
+    font-family = JetBrains Mono
+    background-opacity = 0.95
+    background-blur-radius = 20
+    window-width = 140
+    window-height = 40
   '';
 }
