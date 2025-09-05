cd65d3fb98912c1fa70156195278326b94994ef7
Jcho114 <joseph.cho20@gmail.com>
2025-09-05 09:00:07 -0400

    chore: added nvim config

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@
     ./ghostty.nix
     ./tmux.nix
     ./zoxide.nix
+    ./nvim.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
---
+++ nvim.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+{
+  programs.nvim = {
+    enable = true;
+    extraConfig = ''
+      set number relativenumber
+      set tabstop 2
+      set shiftwidth 2
+      set expandtab
+      set smartindent
+    '';
+  };
+}
