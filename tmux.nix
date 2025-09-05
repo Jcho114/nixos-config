272c96c583a3c507a94ddd33250eb6e66a671947
Jcho114 <joseph.cho20@gmail.com>
2025-09-05 10:34:43 -0400

    chore: added dracula theme for tmux

--- tmux.nix
+++ tmux.nix
@@ -5,5 +5,17 @@
     mouse = true;
     shell = "${pkgs.zsh}/bin/zsh";
     terminal = "xterm-ghostty";
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      yank
+      {
+        plugin = dracula;
+        extraConfig = ''
+          set -g @dracula-show-battery false
+          set -g @dracula-show-powerline true
+          set -g @dracula-refresh-rate 10
+        '';
+      }
+    ];
   };
 }
