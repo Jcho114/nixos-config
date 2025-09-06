4cb23b3149370248c31fdbc67909d65cd8cbdde8
Jcho114 <joseph.cho20@gmail.com>
2025-09-06 16:12:54 -0400

    chore: fixed another config typo

--- hyprland/default.nix
+++ hyprland/default.nix
@@ -14,8 +14,8 @@
       border_size = 2;
       resize_on_border = true;
       layout = "dwindle";
-      "col.active_border": "rgba(33ccffee) rgba(00ff99ee) 45deg";
-      "col.inactive_border": "rgba(595959aa)";
+      "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+      "col.inactive_border" = "rgba(595959aa)";
     };
 
     dwindle = {
